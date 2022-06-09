3469e506b7a44d1169b536ee47035a5cc2d8b665
Bryce <bknguyen@ualberta.ca>
2022-06-09 14:21:05 -0600

    Registering new users

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,37 @@
+const asyncHandler = require("express-async-handler");
+const User = require('../models/userModel');
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please Enter All Fields");
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User Already Exists");
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error("Failed to Create the User");
+    }
+});
+
+module.exports = {registerUser};
--- server/index.js
+++ server/index.js
@@ -7,8 +7,9 @@ dotenv.config();
 const PORT = process.env.PORT || 3001;
 const { trades } = require("./data/data");
 const connectDB = require('./config/db');
-
 connectDB()
+
+const userRoutes = require('./routes/userRoutes');
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -17,18 +18,7 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 // creating an end point "api"
 //  If our react app makes a GET request to the route "api"
 //      respond (using "res") with our JSON data
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
-
-app.get("/api/trade", (req, res) => {
-  res.send(trades);
-});
-app.get("/api/trade/:id", (req, res) => {
-  //console.log(req.params.id);
-  const singleTrade = trades.find(c=> c._id === req.params.id);
-  res.send(singleTrade);
-});
+app.use('/api/user', userRoutes)
 
 
 app.listen(PORT, () => {
