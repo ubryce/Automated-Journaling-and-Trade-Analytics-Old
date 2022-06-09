8f544fe4e44bdd0879387ad697537d06e8bf13c5
Bryce <bknguyen@ualberta.ca>
2022-06-09 14:42:01 -0600

    Creating login api

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -37,4 +37,21 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 });
 
+const authuser = asyncHandler(async(req, res) => {
+    const {email, password} = req.body
+
+    const user = await User.findOne({ email });
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(401);
+        throw new Error("Inavlid Email or Password");
+    }
+});
+
 module.exports = {registerUser};
