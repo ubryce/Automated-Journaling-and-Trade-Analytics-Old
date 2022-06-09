a2be7bbe37aeafb7a2bfdcffb44c252243df483a
Bryce <bknguyen@ualberta.ca>
2022-06-09 09:55:53 -0600

    Signup basic UI

---
+++ client/src/Components/Authentication/Signup.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { VStack } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input"
+import { FormControl, FormLabel } from '@chakra-ui/react';
+
+const Signup = () => {
+
+    const [ show, setShow ] = useState(false);
+    const [ name, setName ] = useState();
+    const [ email, setEmail ] = useState();
+    const [ confirmpassword, setConfirmpassword ] = useState();
+    const [ password, setPassword ] = useState();
+
+    const handleClick = () => setShow(!show);
+
+    const submitHandler = () => {};
+
+    return (
+        <VStack spacing="5px">
+            <FormControl id="first-name" isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input placeholder='Enter Your Name' onChange={(e)=>setName(e.target.value)}/>
+            </FormControl>
+            <FormControl id="email" isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input placeholder='Enter Your Email' onChange={(e)=>setEmail(e.target.value)}/>
+            </FormControl>
+            <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                    <Input type={show ? "text" : 'password'} placeholder='Enter Your Password' onChange={(e)=>setPassword(e.target.value)}/>
+                    <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleClick}>
+                            {show ? "Hide" : "Show"}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+            <FormControl id="password" isRequired>
+                <FormLabel>Confirm Password</FormLabel>
+                <InputGroup size="md">
+                    <Input type={show ? "text" : 'password'} placeholder='Confirm Password' onChange={(e)=>setConfirmpassword(e.target.value)}/>
+                    <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleClick}>
+                            {show ? "Hide" : "Show"}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+
+            <Button colorScheme="blue" width="100%" style={{ marginTop:15 }} onClick={submitHandler}>
+                Sign Up
+            </Button>
+        </VStack>
+    )
+}
+
+export default Signup
--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,10 +1,39 @@
 import React from "react"
+import { Container, Box, Text, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import Login from '../Components/Authentication/Login'
+import Signup from '../Components/Authentication/Signup'
 
 const Homepage = () => {
     return (
-        <div>
-            Home
-        </div>
+        <Container maxW='xl' centerContent>
+            <Box 
+                d="flex" 
+                justifyContent='center'
+                p={3}
+                bg={"white"}
+                w="100%"
+                m="40px 0 15px 0"
+                borderRadius="lg"
+                borderWidth="1px">
+                <Text fontSize="4xl" fontFamily="Work sans" color="black">Automated Journaling</Text>
+            </Box>
+            <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px" >
+            <Tabs variant='soft-rounded'>
+                <TabList mb="1em">
+                    <Tab width="50%">Login</Tab>
+                    <Tab width="50%">Sign Up</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <Login/>
+                    </TabPanel>
+                    <TabPanel>
+                        <Signup/>
+                    </TabPanel>
+                </TabPanels>
+                </Tabs>
+            </Box>
+        </Container>
     );
 };
 
