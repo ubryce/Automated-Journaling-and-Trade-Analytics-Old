00daaa10286d560f1a55a75c30a3bbd25a10c729
Bryce <bknguyen@ualberta.ca>
2022-06-09 10:08:01 -0600

    Update Login.js

--- client/src/Components/Authentication/Login.js
+++ client/src/Components/Authentication/Login.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { VStack } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input"
+import { FormControl, FormLabel } from '@chakra-ui/react';
 
 const Login = () => {
+    
+    const [ show, setShow ] = useState(false);
+    const [ email, setEmail ] = useState();
+    const [ password, setPassword ] = useState();
+
+    const handleClick = () => setShow(!show);
+
+    const submitHandler = () => {};
+
     return (
-        <div>
-            Login
-        </div>
+        <VStack spacing="5px">
+            <FormControl id="email" isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input placeholder='Enter Your Email' onChange={(e)=>setEmail(e.target.value)}/>
+            </FormControl>
+            <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                    <Input type={show ? "text" : 'password'} placeholder='Enter Your Password' onChange={(e)=>setPassword(e.target.value)}/>
+                    <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleClick}>
+                            {show ? "Hide" : "Show"}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+
+            <Button colorScheme="blue" width="100%" style={{ marginTop:15 }} onClick={submitHandler}>
+                Login
+            </Button>
+            <Button variant="solid" colorScheme="yellow" width="100%" style={{ marginTop:15 }} onClick={() => {
+                setEmail("guest@example.com");
+                setPassword("123456");
+            }}>
+                Guest
+            </Button>
+        </VStack>
     )
 }
 
