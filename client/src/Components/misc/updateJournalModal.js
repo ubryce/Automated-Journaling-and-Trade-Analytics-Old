5a0c96b1f50c374de596680162bc2f54433ab681
Bryce <bknguyen@ualberta.ca>
2022-06-15 12:57:09 -0600

    Journal Modal Search user function

--- client/src/Components/misc/updateJournalModal.js
+++ client/src/Components/misc/updateJournalModal.js
@@ -34,8 +34,35 @@ const UpdateJournalModal = ({fetchAgain, setFetchAgain}) => {
 
     const { selectedJournal, setSelectedJournal, user } = JournalState();
 
-    const handleSearch = () => {
+    const handleSearch = async (query) => {
+        setSearch(query);
+        if (!query) {
+          return;
+        }
+    
+        try {
+          setLoading(true);
 
+          const config = {
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          };
+
+          const { data } = await axios.get(`/api/user?search=${search}`, config);
+          setLoading(false);
+          setSearchResult(data);
+        } catch (error) {
+          toast({
+            title: "Error Occured!",
+            description: "Failed to Load the Search Results",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom-left",
+          });
+          setLoading(false);
+        }
     };
 
     const handleRename = async () => {
