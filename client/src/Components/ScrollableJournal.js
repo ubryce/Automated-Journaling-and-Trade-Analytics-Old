8c07ad1100e3feb8ea7c2205ee11584efad0142d
Bryce <bknguyen@ualberta.ca>
2022-06-16 11:02:32 -0600

    Changing debugging with Scrollable Journal

--- client/src/Components/ScrollableJournal.js
+++ client/src/Components/ScrollableJournal.js
@@ -5,7 +5,7 @@ const ScrollableJournal = ({trades}) => {
     return (
         <ScrollableFeed>
             {trades && trades.map((m,i) => {
-                console.log(m.thread[i]);
+                console.log(m.thread);
                 <div style={{display:"flex"}} key={m._id}>
                     
                 </div>
