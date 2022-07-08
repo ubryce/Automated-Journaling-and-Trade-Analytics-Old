6a9ae3ca743b225ec3baabca7463a78c5182169b
Bryce <bknguyen@ualberta.ca>
2022-07-07 22:30:11 -0600

    new journal modal

--- client/src/components/MyJournals.jsx
+++ client/src/components/MyJournals.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useStateContext } from '../contexts/ContextProvider';
-import { Button, JournalModal, Header } from '../components';
+import {  JournalModal, Header } from '../components';
 
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -11,6 +11,14 @@ import Select from '@mui/material/Select';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 import { PlusIcon } from '@heroicons/react/outline'
 
@@ -23,6 +31,7 @@ const MyJournals = ({fetchAgain}) => {
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    
 
     const fetchJournals = async () => {
         try {
@@ -55,24 +64,32 @@ const MyJournals = ({fetchAgain}) => {
     return (
         <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
             <Header category="Page" title="Journals" />
-            {/* <JournalModal /> */}
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Text in a modal
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </Typography>
-                </Box>
-            </Modal>
+            <Dialog open={open} onClose={handleClose}> 
+                <DialogTitle>New Journal</DialogTitle>
+                <DialogContent>
+                <div>
+                    <TextField
+                    label="Journal Name"
+                    id="standard-size-normal"
+                    defaultValue=""
+                    variant="standard"
+                    />
+                </div>
+                <div>
+                    <TextField
+                        label="Journal Description"
+                        id="standard-size-normal"
+                        defaultValue=""
+                        variant="standard"
+                    />
+                </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleClose}>Create</Button>
+                </DialogActions>
+            </Dialog>
             <div className="md:flex items-center justify-end md:flex-1">
-                <Button customFunc={() => setAddJournal(true)} color="white" bgColor={currentColor} text="Create a Journal" borderRadius="10px" size="md" />
                 <PlusIcon onClick={handleOpen} className="h-6 w-6 text-gray-600 hover:drop-shadow-xl cursor-pointer" aria-hidden="true" />
             </div>
             <div>
