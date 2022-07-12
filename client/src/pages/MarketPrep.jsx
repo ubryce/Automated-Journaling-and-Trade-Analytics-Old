47c1c26bd306bdb604b851e393387ab372165f56
Bryce <bknguyen@ualberta.ca>
2022-07-12 00:39:28 -0600

    marketprep basic ui

--- client/src/pages/MarketPrep.jsx
+++ client/src/pages/MarketPrep.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Header } from '../components';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const MarketPrep = () => {
   return (
-    <div>MarketPrep</div>
+    <div className='mt-12'>
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="Page" title="Exchanges" />
+           
+        </div>
+    </div>
   )
 }
 
