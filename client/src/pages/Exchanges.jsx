d7f4954184d7e2e61f1369761791fa537fe02547
Bryce <bknguyen@ualberta.ca>
2022-07-18 11:25:19 -0600

    combine header and selected journal/exchange

--- client/src/pages/Exchanges.jsx
+++ client/src/pages/Exchanges.jsx
@@ -11,9 +11,11 @@ const Exchanges = () => {
     return (
         <div className='mt-12'>
             <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-                <Header category="Page" title="Exchanges" />
-                {user && <MyExchanges fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
-                {user && selectedExchange && <SingleExchange fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
+                <div className="flex justify-between">
+                    <Header category="Page" title="Exchanges" />
+                    {user && <MyExchanges fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
+                </div>
+                    {user && selectedExchange && <SingleExchange fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
             </div>
         </div>
     )
--- client/src/pages/Journals.jsx
+++ client/src/pages/Journals.jsx
@@ -11,8 +11,10 @@ const Journals = () => {
   return (
     <div className='mt-12'>
         <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+          <div className="flex justify-between">
             <Header category="Page" title="Journals" />
             {user && <MyJournals fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
+          </div>
             {user && selectedJournal && <SingleJournal fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
         </div>
     </div>
