69c04ba49b2706744505c3ec97ef9d02d944f0a2
Bryce <bknguyen@ualberta.ca>
2022-07-27 17:58:25 -0600

    Removing unused code

--- client/src/components/SingleJournal.jsx
+++ client/src/components/SingleJournal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import axios from 'axios';
 import { useStateContext } from '../contexts/ContextProvider';
 import { SubHeader } from './';
