a001e688e365ac9b16fb6bb033c38df323bc5094
Bryce <bknguyen@ualberta.ca>
2022-11-20 19:28:34 -0700

    Removing unused data

--- client/src/pages/Ecommerce.jsx
+++ client/src/pages/Ecommerce.jsx
@@ -6,7 +6,6 @@ import { IoIosMore } from 'react-icons/io';
 import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
 import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
-import product9 from '../data/product9.jpg';
 
 const Ecommerce = () => {
   const { currentColor } = useStateContext();
