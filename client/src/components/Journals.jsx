2f64167c658d587a50dbe77e9fbe96b325baf019
Bryce <bknguyen@ualberta.ca>
2022-11-23 13:33:12 -0700

    fetch journals function

--- client/src/components/Journals.jsx
+++ client/src/components/Journals.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import axios from 'axios';
@@ -6,6 +6,28 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const Journals = () => {
   const { user, journals, setJournals, selectedJournal, setSelectedJournal } = useStateContext()
+
+  const fetchJournals = async () => {
+    try {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${user.token}`,
+            },
+        };
+
+        const {data} = await axios.get("/api/journal", config);
+        setJournals(data);
+        console.log(data)
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchJournals();
+    setSelectedJournal();
+  }, []);
+
   return (
     <div>
       <Grid container spacing={3}>
