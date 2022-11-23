948d87efafdbd72c856bbf2a587c082822acf8a9
Bryce <bknguyen@ualberta.ca>
2022-11-23 13:44:32 -0700

    journal list boiler

--- client/src/components/Journals.jsx
+++ client/src/components/Journals.jsx
@@ -4,6 +4,13 @@ import Paper from '@mui/material/Paper';
 import axios from 'axios';
 import { useStateContext } from '../contexts/ContextProvider';
 
+import Link from '@mui/material/Link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
 const Journals = () => {
   const { user, journals, setJournals, selectedJournal, setSelectedJournal } = useStateContext()
 
@@ -21,11 +28,14 @@ const Journals = () => {
     } catch (error) {
         console.log(error);
     }
+  };
+
+  function preventDefault(event) {
+    event.preventDefault();
   }
 
   useEffect(() => {
     fetchJournals();
-    setSelectedJournal();
   }, []);
 
   return (
@@ -33,7 +43,32 @@ const Journals = () => {
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-            
+          Journals
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>Ship To</TableCell>
+                <TableCell>Payment Method</TableCell>
+                <TableCell align="right">Sale Amount</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {journals.map((journal) => (
+                <TableRow key={journal._id}>
+                  <TableCell>{journal.journalName}</TableCell>
+                  <TableCell>{journal.journalDescription}</TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell align="right">{`$${journal.amount}`}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+            See more orders
+          </Link>
           </Paper>
         </Grid>
       </Grid>
