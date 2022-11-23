6f6d97b87997dd78d1b60c015a0365fd4216c240
Bryce <bknguyen@ualberta.ca>
2022-11-23 09:11:22 -0700

    navigate from home to dashboard if logged in

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      const user = JSON.parse(localStorage.getItem("userInfo"));
+      if (user) navigate('/dashboard');
+  }, [navigate]);
+
   return (
     <div>
       Home
