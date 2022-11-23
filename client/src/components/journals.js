e52825a7d37c97d642dcc02760a5cb71434b761e
Bryce <bknguyen@ualberta.ca>
2022-11-23 12:53:00 -0700

    journals grid layout

--- client/src/components/journals.js
+++ client/src/components/journals.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
 const journals = () => {
   return (
-    <div>journals</div>
+    <div>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+            
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
   )
 }
 
