771e7fd98dc1ce92adc6d5e05617d5029c40e44e
Bryce <bknguyen@ualberta.ca>
2022-11-24 13:47:47 -0700

    home button to navigate to dashboard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 const Home = () => {
+  const navigate = useNavigate();
   
   return (
     <div>
+      <Button variant="contained" onClick={() => navigate('/dashboard')}>+Dashboard</Button>
       Home
     </div>
   );
