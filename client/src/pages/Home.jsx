fe74ed550deac04185daa96c8dc667d78423609f
Bryce <bknguyen@ualberta.ca>
2022-11-27 14:24:00 -0700

    change home tag

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,7 +10,6 @@ const Home = () => {
       <Button variant="contained" onClick={() => navigate('/dashboard')}>Dashboard</Button>
       <Button variant="contained" onClick={() => navigate('/signup')}>Sign Up</Button>
       <Button variant="contained" onClick={() => navigate('/login')}>Login</Button>
-      Home
     </div>
   );
 }
