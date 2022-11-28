5cecf32962e6f17bffd430e92f057a2a50b44db0
Bryce <bknguyen@ualberta.ca>
2022-11-28 12:51:41 -0700

    add an exchange route

--- client/src/components/Exchange.jsx
+++ client/src/components/Exchange.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 
 import Exchanges from './Exchanges';
+import ExchangeAdd from './ExchangeAdd';
 
 
 const Exchange = () => {
   return (
     <Routes>
         <Route path={'/'} element={<Exchanges/>} /> 
+        <Route path={'/add'} element={<ExchangeAdd/>} /> 
     </Routes>
   )
 }
---
+++ client/src/components/ExchangeAdd.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExchangeAdd = () => {
+  return (
+    <div>ExchangeAdd</div>
+  )
+}
+
+export default ExchangeAdd
