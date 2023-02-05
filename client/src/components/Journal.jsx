242b26e22f379a150bc8e44b15021d5548330a05
Bryce Nguyen <bknguyen@ualberta.ca>
2023-02-05 16:19:52 -0700

    Add updated trade page

--- client/src/components/Journal.jsx
+++ client/src/components/Journal.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import Journals from './Journals'
 import JournalAdd from './JournalAdd';
 import JournalView from './JournalView';
+import TradeAdd from './TradeAdd';
 
 const Journal = () => {
   return (
     <Routes>
         <Route path={'/'} element={<Journals/>} /> 
         <Route path={'/add'} element={<JournalAdd/>} /> 
-        <Route path={'/:id'} element={<JournalView/>} /> 
+        <Route path={'/:id'} element={<JournalView/>} />
+        <Route path={'/:id/add'} element={<TradeAdd/>} />
     </Routes>
   )
 }
--- client/src/components/JournalView.jsx
+++ client/src/components/JournalView.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { useStateContext } from '../contexts/ContextProvider';
-import { useNavigate, Link } from 'react-router-dom';
+import {useStateContext} from '../contexts/ContextProvider';
+import {useNavigate, Link} from 'react-router-dom';
 import axios from 'axios';
 
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 
 import Links from '@mui/material/Link';
 import Table from '@mui/material/Table';
@@ -22,83 +22,85 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 
 const JournalView = () => {
-  const { selectedJournal, selectedTrades, setSelectedTrades, user } = useStateContext()
-  const navigate = useNavigate();
+    const {selectedJournal, selectedTrades, setSelectedTrades, user} = useStateContext()
+    const navigate = useNavigate();
 
-  const fetchTrades = async () => {
-    try {
-        const config = {
-            headers: {
-                Authorization: `Bearer ${user.token}`,
-            },
-        };
-        console.log(selectedJournal._id)
-        const {data} = await axios.get(`/api/trade/${selectedJournal._id}`, config);
-        setSelectedTrades(data);
-        console.log(data)
-    } catch (error) {
-        console.log(error);
+    const fetchTrades = async () => {
+        try {
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
+            };
+            console.log(selectedJournal._id)
+            const {data} = await axios.get(`/api/trade/${selectedJournal._id}`, config);
+            setSelectedTrades(data);
+            console.log(data)
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    function preventDefault(event) {
+        event.preventDefault();
     }
-  };
-
-  function preventDefault(event) {
-    event.preventDefault();
-  }
 
 
-  useEffect(() => {
-    fetchTrades();
-  }, []);
+    useEffect(() => {
+        fetchTrades();
+    }, []);
 
-  return (
-    <Grid container spacing={3}>
-        <Grid item xs={12}>
-            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                <Tooltip title="Back">
-                    <IconButton onClick={() => navigate("/dashboard/journal")}>
-                    <ArrowBackIosNewIcon />
-                    </IconButton>
-                </Tooltip>
-                <Tooltip title="Add Trade">
-                    <IconButton onClick={() => navigate("/dashboard/journal")}>
-                        <AddIcon/>
-                    </IconButton>
-                </Tooltip>
-                Name: 
-                {selectedJournal.journalName}
-                Description: 
-                {selectedJournal.journalDescription}
-                <Table size="small">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Name</TableCell>
-                      <TableCell>Description</TableCell>
-                      <TableCell>Ship To</TableCell>
-                      <TableCell>Payment Method</TableCell>
-                      <TableCell align="right">Sale Amount</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {selectedTrades?.map((trades) => (
-                      <TableRow key={trades._id}>
-                        <Link >
-                          <TableCell>{trades.thread[0].content}</TableCell>
-                          <TableCell></TableCell>
-                          <TableCell></TableCell>
-                          <TableCell></TableCell>
-                          {/* <TableCell align="right">{`$${trades.amount}`}</TableCell> */}
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Paper sx={{p: 2, display: 'flex', flexDirection: 'column'}}>
+                    <Tooltip title="Back">
+                        <IconButton onClick={() => navigate("/dashboard/journal")}>
+                            <ArrowBackIosNewIcon/>
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Add Trade">
+                        <Link to={`/dashboard/journal/${selectedJournal._id}/add`}>
+                            <IconButton>
+                                <AddIcon/>
+                            </IconButton>
                         </Link>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-                <Links color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-                  See more orders
-                </Links>
-            </Paper>
+                    </Tooltip>
+                    Name:
+                    {selectedJournal.journalName}
+                    Description:
+                    {selectedJournal.journalDescription}
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Description</TableCell>
+                                <TableCell>Ship To</TableCell>
+                                <TableCell>Payment Method</TableCell>
+                                <TableCell align="right">Sale Amount</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {selectedTrades?.map((trades) => (
+                                <TableRow key={trades._id}>
+                                    <Link>
+                                        <TableCell>{trades.thread[0].content}</TableCell>
+                                        <TableCell></TableCell>
+                                        <TableCell></TableCell>
+                                        <TableCell></TableCell>
+                                        {/* <TableCell align="right">{`$${trades.amount}`}</TableCell> */}
+                                    </Link>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                    <Links color="primary" href="#" onClick={preventDefault} sx={{mt: 3}}>
+                        See more orders
+                    </Links>
+                </Paper>
+            </Grid>
         </Grid>
-    </Grid>
-  )
+    )
 }
 
 export default JournalView
