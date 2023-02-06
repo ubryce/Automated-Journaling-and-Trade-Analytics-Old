b5f1fbfa11856d9feea10b83a139d6f7d7135f97
Bryce Nguyen <bknguyen@ualberta.ca>
2023-02-06 08:17:30 -0700

    Update addtrade page

--- client/src/components/TradeAdd.jsx
+++ client/src/components/TradeAdd.jsx
@@ -34,10 +34,34 @@ const TradeAdd = () => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
 
-        console.log({
-            name: data.get('name'),
-            description: data.get('description'),
-        });
+        const tradeData = {
+            user: user._id,
+            journalId: selectedJournal._id,
+            openDate: new Date(data.get("openDate")),
+            closeDate: new Date(data.get("closeDate")),
+            side: data.get("side"),
+            exchange: data.get("exchange"),
+            symbol: data.get("symbol"),
+            avgEntry: data.get("avgEntry"),
+            stop: data.get("stop"),
+            target: data.get("target"),
+            exit: data.get("exit"),
+            size: data.get("size"),
+            sizeFiat: data.get("sizeFiat"),
+            walletBalance: data.get("walletBalance"),
+            accRisk: data.get("accRisk"),
+            confidence: data.get("confidence"),
+            plannedRisk: data.get("plannedRisk"),
+            finalRisk: data.get("finalRisk"),
+            isOpen: data.get("isOpen"),
+            tags: data.get("tags").split(','),
+            thread: [
+                {
+                    content: data.get("threadContent"),
+                    picture: data.get("threadPicture"),
+                },
+            ],
+        };
 
         const config = {
             headers: {
@@ -46,15 +70,10 @@ const TradeAdd = () => {
         };
 
         const {data2} = await axios.post(
-            "/api/journal/create",
-            {
-                name: data.get('name'),
-                description: data.get('description'),
-            },
-            config
+            "/api/trade", tradeData, config
         ).then((response) => {
             console.log(response.data)
-            navigate('/dashboard/journal');
+            navigate(`/dashboard/journal/${selectedJournal._id}`);
         }, (error) => {
             console.log(error.message)
         }) || {}
@@ -85,20 +104,168 @@ const TradeAdd = () => {
                                                 <TextField
                                                     required
                                                     fullWidth
-                                                    id="name"
-                                                    label="Name"
-                                                    name="name"
-                                                    autoComplete="name"
+                                                    id="exchange"
+                                                    label="Exchange"
+                                                    name="exchange"
+                                                    autoComplete="exchange"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="opendate"
+                                                    label="Open Date"
+                                                    id="opendate"
+                                                    autoComplete="opendate"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="closedate"
+                                                    label="Close Date"
+                                                    id="closedate"
+                                                    autoComplete="closedate"
                                                 />
                                             </Grid>
                                             <Grid item xs={12}>
                                                 <TextField
                                                     required
                                                     fullWidth
-                                                    name="description"
-                                                    label="Description"
-                                                    id="description"
-                                                    autoComplete="description"
+                                                    name="side"
+                                                    label="Side"
+                                                    id="side"
+                                                    autoComplete="side"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    required
+                                                    fullWidth
+                                                    name="symbol"
+                                                    label="Symbol"
+                                                    id="symbol"
+                                                    autoComplete="symbol"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    required
+                                                    fullWidth
+                                                    name="avgEntry"
+                                                    label="Entry"
+                                                    id="avgEntry"
+                                                    autoComplete="avgEntry"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="stop"
+                                                    label="Stop"
+                                                    id="stop"
+                                                    autoComplete="stop"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="target"
+                                                    label="Target"
+                                                    id="target"
+                                                    autoComplete="target"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="exit"
+                                                    label="Exit"
+                                                    id="exit"
+                                                    autoComplete="exit"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    required
+                                                    fullWidth
+                                                    name="size"
+                                                    label="Size"
+                                                    id="size"
+                                                    autoComplete="size"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="sizeFiat"
+                                                    label="Size Fiat"
+                                                    id="sizeFiat"
+                                                    autoComplete="sizeFiat"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="walletBalance"
+                                                    label="Wallet Balance"
+                                                    id="walletBalance"
+                                                    autoComplete="walletBalance"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="accRisk"
+                                                    label="Account Risk"
+                                                    id="accRisk"
+                                                    autoComplete="accRisk"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    required
+                                                    fullWidth
+                                                    name="confidence"
+                                                    label="Confidence"
+                                                    id="confidence"
+                                                    autoComplete="confidence"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="plannedRisk"
+                                                    label="Planned Risk"
+                                                    id="plannedRisk"
+                                                    autoComplete="plannedRisk"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="finalRisk"
+                                                    label="Final Risk"
+                                                    id="finalRisk"
+                                                    autoComplete="finalRisk"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="isOpen"
+                                                    label="Open"
+                                                    id="isOpen"
+                                                    autoComplete="isOpen"
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <TextField
+                                                    fullWidth
+                                                    name="tags"
+                                                    label="Tags"
+                                                    id="tags"
+                                                    autoComplete="tags"
                                                 />
                                             </Grid>
                                         </Grid>
