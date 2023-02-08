6fad85f9c6fdbcf8a8840e37fc66c2272f343748
Bryce Nguyen <bknguyen@ualberta.ca>
2023-02-07 21:17:05 -0700

    update trade data into db

--- server/controllers/tradeControllers.js
+++ server/controllers/tradeControllers.js
@@ -10,11 +10,13 @@ const sendTrade = asyncHandler( async (req, res) => {
         console.log("Invalid data passed into request");
         return res.sendStatus(400);
     }
+    console.log(req.body);
 
     var newTrade = {
         user: req.user._id,
         journal: journalId,
         thread: thread,
+        size: req.body.size
     }
 
     try {
