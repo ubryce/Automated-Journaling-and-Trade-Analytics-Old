6ab744c8d49b26a400b19935759355fc783fdfcf
Bryce Nguyen <bknguyen@ualberta.ca>
2023-02-17 06:34:55 -0700

    update tag model

--- server/models/tagModel.js
+++ server/models/tagModel.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const tagModel = mongoose.Schema(
     {
         users: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
-        tag: {type: String}
+        tag: {type: String},
+        tagType: {type: String, enum: ["setup", "mistake"]}
     }
-
 );
 
 const Tag = mongoose.model("Tag", tagModel);
