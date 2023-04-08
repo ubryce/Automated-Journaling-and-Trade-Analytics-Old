b4ae1e3e131730d68e0f7aad72e73ced9e6d25c6
Bryce Nguyen <bknguyen@ualberta.ca>
2023-04-08 13:51:46 -0600

    feature: create updateorcreatetrade in trade controllers

--- server/controllers/tradeControllers.js
+++ server/controllers/tradeControllers.js
@@ -6,7 +6,6 @@ const User = require('../models/userModel');
 const sendTrade = asyncHandler( async (req, res) => {
 
     // TODO Check if all required inputs are sent with proper data types
-    // TODO check if trade is closed, if so then calculate the pnl and append it to trade
     // Remove latest trade
     // if (!thread || !journalId) {
     //     console.log("Invalid data passed into request");
@@ -66,6 +65,71 @@ const sendTrade = asyncHandler( async (req, res) => {
     }
 });
 
+const updateOrCreateTrade = asyncHandler(async (req, res) => {
+    const tradeId = req.body.tradeId;
+
+    const newTrade = {
+        user: req.user._id,
+        journal: req.body.journalId,
+        openDate: req.body.openDate,
+        closeDate: req.body.closeDate,
+        side: req.body.side,
+        exchange: req.body.exchange,
+        symbol: req.body.symbol,
+        avgEntry: parseInt(req.body.avgEntry),
+        stop: parseInt(req.body.stop),
+        target: parseInt(req.body.target),
+        exit: parseInt(req.body.exit),
+        size: parseInt(req.body.size),
+        sizeFiat: parseInt(req.body.sizeFiat),
+        pnl: parseInt(req.body.pnl),
+        walletBalance: parseInt(req.body.walletBalance),
+        accRisk: parseInt(req.body.accRisk),
+        confidence: parseInt(req.body.confidence),
+        execution: parseInt(req.body.execution),
+        entryRating: parseInt(req.body.entryRating),
+        management: parseInt(req.body.management),
+        exitRating: parseInt(req.body.exitRating),
+        plannedRisk: parseInt(req.body.plannedRisk),
+        finalRisk: parseInt(req.body.finalRisk),
+        isOpen: req.body.isOpen,
+        tags: req.body.tags,
+        thread: req.body.thread,
+    };
+    console.log(newTrade);
+
+    try {
+        let trade;
+
+        if (tradeId) {
+            trade = await Trade.findByIdAndUpdate(tradeId, tradeData, {
+                new: true,
+            });
+        } else {
+            // Create new trade
+            trade = await Trade.create(tradeData);
+        }
+
+        trade = await trade.populate("user", "name");
+        trade = await trade.populate("journal");
+        trade = await User.populate(trade, {
+            path: "journal.users",
+            select: "name email",
+        });
+
+        if (!tradeId) {
+            await Journal.findByIdAndUpdate(req.body.journalId, {
+                latestTrade: trade,
+            });
+        }
+
+        res.json(trade);
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+});
+
 const allTrades = asyncHandler( async (req, res) => {
     try {
         const trades = await Trade.find({journal:req.params.journalId})
@@ -92,4 +156,4 @@ const accessTrade = asyncHandler(async (req, res) => {
     } 
 })
 
-module.exports = { sendTrade, allTrades, accessTrade };
+module.exports = { sendTrade, updateOrCreateTrade, allTrades, accessTrade };
--- server/routes/tradeRoutes.js
+++ server/routes/tradeRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
-const { sendTrade, allTrades, accessTrade } = require('../controllers/tradeControllers')
+const { sendTrade, updateOrCreateTrade, allTrades, accessTrade } = require('../controllers/tradeControllers')
 
 const router = express.Router();
 
-router.route('/').post(protect, sendTrade);
+router.route('/').post(protect, updateOrCreateTrade);
 router.route('/:journalId').get(protect, allTrades);
 router.route('/single/:tradeId').get(protect, accessTrade);
 
