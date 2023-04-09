4abc9f482eb498454613e27a8cb7b48d871ff39e
Bryce Nguyen <bknguyen@ualberta.ca>
2023-04-09 16:03:49 -0600

    feature: update trade routes fully functional

--- server/controllers/tradeControllers.js
+++ server/controllers/tradeControllers.js
@@ -3,70 +3,9 @@ const Journal = require("../models/journalModel");
 const Trade = require('../models/tradeModel');
 const User = require('../models/userModel');
 
-const sendTrade = asyncHandler( async (req, res) => {
-
-    // TODO Check if all required inputs are sent with proper data types
-    // Remove latest trade
-    // if (!thread || !journalId) {
-    //     console.log("Invalid data passed into request");
-    //     return res.sendStatus(400);
-    // }
-
-    const newTrade = {
-        user: req.user._id,
-        journal: req.body.journalId,
-        openDate: req.body.openDate,
-        closeDate: req.body.closeDate,
-        side: req.body.side,
-        exchange: req.body.exchange,
-        symbol: req.body.symbol,
-        avgEntry: parseInt(req.body.avgEntry),
-        stop: parseInt(req.body.stop),
-        target: parseInt(req.body.target),
-        exit: parseInt(req.body.exit),
-        size: parseInt(req.body.size),
-        sizeFiat: parseInt(req.body.sizeFiat),
-        pnl: parseInt(req.body.pnl),
-        walletBalance: parseInt(req.body.walletBalance),
-        accRisk: parseInt(req.body.accRisk),
-        confidence: parseInt(req.body.confidence),
-        execution: parseInt(req.body.execution),
-        entryRating: parseInt(req.body.entryRating),
-        management: parseInt(req.body.management),
-        exitRating: parseInt(req.body.exitRating),
-        plannedRisk: parseInt(req.body.plannedRisk),
-        finalRisk: parseInt(req.body.finalRisk),
-        isOpen: req.body.isOpen,
-        tags: req.body.tags,
-        thread: req.body.thread,
-    };
-    console.log(newTrade);
-
-    try {
-
-        var trade = await Trade.create(newTrade);
-
-        trade = await trade.populate("user", "name");
-        trade = await trade.populate("journal");
-        trade = await User.populate(trade, {
-            path: 'journal.users',
-            select: "name email"
-        });
-
-        await Journal.findByIdAndUpdate(req.body.journalId, {
-            latestTrade: trade,
-        });
-        console.log(trade);
-
-        res.json(trade);
-    } catch (error) {
-        res.status(400);
-        throw new Error(error.message);
-    }
-});
-
+// TODO Check if all required inputs are sent with proper data types
 const updateOrCreateTrade = asyncHandler(async (req, res) => {
-    const tradeId = req.body.tradeId;
+    const tradeId = req.params.tradeId;
 
     const tradeData = {
         user: req.user._id,
@@ -102,13 +41,21 @@ const updateOrCreateTrade = asyncHandler(async (req, res) => {
         let trade;
 
         if (tradeId) {
-            trade = await Trade.findByIdAndUpdate(tradeId, tradeData, {
-                new: true,
-            });
+            trade = await Trade.findById(tradeId);
+
+            if (!trade) {
+                res.status(404);
+                throw new Error("Trade not found");
+            }
+
+            // Update trade fields with tradeData
+            Object.assign(trade, tradeData);
+
+            // Save the updated trade
+            await trade.save();
+
         } else {
-            console.log("ran")
             trade = await Trade.create(tradeData);
-            console.log("afr")
         }
 
         trade = await trade.populate("user", "name");
