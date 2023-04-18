2c2996f3029e370af8438ac4a86d14b235f4cc97
Bryce Nguyen <bknguyen@ualberta.ca>
2023-04-17 21:58:28 -0600

    fix todos

--- client/src/components/TradeAdd.jsx
+++ client/src/components/TradeAdd.jsx
@@ -26,7 +26,8 @@ const theme = createTheme();
 // TODO remove thread
 // TODO remove tag
 // TODO bug when tags is long
-// TODO allow trade view
+// TODO trade tag edit bug
+// TODO empty selectedtrade
 const TradeAdd = () => {
     const {selectedTrade, selectedJournal, user, tags, setTags} = useStateContext();
     const navigate = useNavigate();
