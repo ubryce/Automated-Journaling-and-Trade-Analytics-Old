f4b80cf76db520d89ad9bc2ba706cd46eae9f482
Bryce Nguyen <bknguyen@ualberta.ca>
2023-04-30 19:46:44 -0600

    refactor: move rating related functions into its component

--- client/src/components/TradeEdit/RatingTextField.jsx
+++ client/src/components/TradeEdit/RatingTextField.jsx
@@ -3,7 +3,19 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 
-const RatingTextField = ({ name, label, defaultValue, menuItems }) => (
+const ratingItems = [
+    {value: 1, label: "1"},
+    {value: 2, label: "2"},
+    {value: 3, label: "3"},
+    {value: 4, label: "4"},
+    {value: 5, label: "5"},
+    {value: 6, label: "6"},
+    {value: 7, label: "7"},
+    {value: 8, label: "8"},
+    {value: 9, label: "9"},
+    {value: 10, label: "10"},
+]
+const RatingTextField = ({ name, label, defaultValue }) => (
     <Grid item xs={12}>
         <TextField
             fullWidth
@@ -14,7 +26,7 @@ const RatingTextField = ({ name, label, defaultValue, menuItems }) => (
             select
             defaultValue={defaultValue}
         >
-            {menuItems.map((item) => (
+            {ratingItems.map((item) => (
                 <MenuItem key={item.value} value={item.value}>
                     {item.label}
                 </MenuItem>
